9ebf414f4eb4fc3c32a579b1af5c8dc7bae901b2
ufnaimah <222313302@stis.ac.id>
2025-07-13 14:58:52 +0700

    Update CORS for Vercel frontend

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,16 @@
 <?php
+
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://web-bps-frontend-seven.vercel.app',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
 ];
+
